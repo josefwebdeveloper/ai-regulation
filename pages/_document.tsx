3d7ba7f0b84a530c9402f2c91d3d474a074958d7
Josef Shainskiy <josef2011@gmail.com>
2025-06-20 22:51:59 +0300

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -15,6 +15,20 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           }}
         />
         
+        {/* Hotjar Tracking Code */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:5200000,hjsv:6}; // Replace 5200000 with your actual Hotjar Site ID
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`
+          }}
+        />
+        
         {/* SEO and Performance */}
         <meta charSet="utf-8" />
         <meta name="robots" content="index, follow" />
