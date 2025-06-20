f758561bee56acace04d247716933f96af1b8ce0
Josef Shainskiy <josef2011@gmail.com>
2025-06-20 17:43:27 +0300

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,17 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PXSHRT45');`
+          }}
+        />
+        
         {/* SEO and Performance */}
         <meta charSet="utf-8" />
         <meta name="robots" content="index, follow" />
@@ -51,6 +62,16 @@ export default function Document() {
         />
       </Head>
       <body className="antialiased">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PXSHRT45"
+            height="0" 
+            width="0" 
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        
         <Main />
         <NextScript />
       </body>
